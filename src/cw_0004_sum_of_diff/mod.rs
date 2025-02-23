43e7b1538467568c1ff39dbce54ed9667c78402d
Kurt Schwind <kurt.schwind@gmail.com>
2025-02-23 10:23:17 -0600

    minor

--- src/cw_0004_sum_of_diff/mod.rs
+++ src/cw_0004_sum_of_diff/mod.rs
@@ -1,5 +1,5 @@
 /*
- * 
+ *
 * https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/train/rust
 */
 
@@ -7,11 +7,11 @@ fn sum_of_differences(arr: &[i8]) -> Option<i8> {
     if arr.len() < 2 {
         return None;
     }
-    let mut vec: Vec<i8> = arr.to_vec().into_iter().rev().collect();
+    let vec: Vec<i8> = arr.iter().copied().rev().collect();
     let mut prev = vec[0];
     let mut retval = 0;
     for i in vec {
-        retval += (i - prev).abs() as i8;
+        retval += (i - prev).abs();
         prev = i;
     }
     Some(retval)
@@ -21,7 +21,6 @@ fn sum_of_differences(arr: &[i8]) -> Option<i8> {
 mod tests {
     use super::*;
 
-
     const ERR_MSG: &str = "\nYour results (left) did not match the expected output (right)";
     #[test]
     fn returns_expected() {
@@ -29,10 +28,9 @@ mod tests {
         assert_eq!(sum_of_differences(&[0]), None, "{}", ERR_MSG);
         assert_eq!(sum_of_differences(&[1]), None, "{}", ERR_MSG);
         assert_eq!(sum_of_differences(&[-1]), None, "{}", ERR_MSG);
-        assert_eq!(sum_of_differences(&[1,2,10]), Some(9), "{}", ERR_MSG);
-        assert_eq!(sum_of_differences(&[-3,-2,-1]), Some(2), "{}", ERR_MSG);
-        assert_eq!(sum_of_differences(&[1,1,1, 1]), Some(0), "{}", ERR_MSG);
+        assert_eq!(sum_of_differences(&[1, 2, 10]), Some(9), "{}", ERR_MSG);
+        assert_eq!(sum_of_differences(&[-3, -2, -1]), Some(2), "{}", ERR_MSG);
+        assert_eq!(sum_of_differences(&[1, 1, 1, 1]), Some(0), "{}", ERR_MSG);
         assert_eq!(sum_of_differences(&[17, -17]), Some(34), "{}", ERR_MSG);
     }
 }
-
